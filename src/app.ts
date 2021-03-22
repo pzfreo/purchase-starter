0bbef941590fcca2c0d878354e56880a6888353f
Paul Fremantle <pzfreo@github.com>
2021-03-22 06:36:52 +0000

    fix dc

--- src/app.ts
+++ src/app.ts
@@ -14,27 +14,27 @@ app.use(
 app.use(bodyParser.json());
 
 
-// app.use(function errorHandler(
-//   err: unknown,
-//   req: express.Request,
-//   res: express.Response,
-//   next: express.NextFunction
-// ): express.Response | void {
-//   if (err instanceof ValidateError) {
-//     console.warn(`Caught Validation Error for ${req.path}:`, err.fields);
-//     return res.status(422).json({
-//       message: "Validation Failed",
-//       details: err?.fields,
-//     });
-//   }
-//   if (err instanceof Error) {
-//     return res.status(500).json({
-//       message: "Internal Server Error",
-//     });
-//   }
+app.use(function errorHandler(
+  err: unknown,
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+): express.Response | void {
+  if (err instanceof ValidateError) {
+    console.warn(`Caught Validation Error for ${req.path}:`, err.fields);
+    return res.status(422).json({
+      message: "Validation Failed",
+      details: err?.fields,
+    });
+  }
+  if (err instanceof Error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
 
-//   next();
-// });
+  next();
+});
 
 RegisterRoutes(app);
 
